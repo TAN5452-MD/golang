250e4f64e548fa22dfa8e191ccc137d4b7791480
TAN5452-MD <seesawgzz@gmail.com>
2023-04-17 18:09:12 +0800

    1

--- golang/main.go
+++ golang/main.go
@@ -1,15 +1,25 @@
 package main
 
-
 import (
-	"Pack1"
+	"fmt"
+	"regexp"
+	"strconv"
 )
 
-
+type T struct {
+	a int
+	b float32
+	c string
+}
 
 func main() {
-	var a = new(Pack.MyStruct)
-	a.i = 1
+	t := &T{7, -2.35, "abc\tdef"}
+	fmt.Println(t)
+}
 
-
+func (t *T) String() string {
+	var pat = "\t"
+	re := regexp.MustCompile(pat)
+	str := re.ReplaceAllString(t.c, "\\t")
+	return "" + strconv.Itoa(t.a) + "/" + strconv.FormatFloat(float64(t.b), 'g', -1, 32) + "/" + str + ""
 }
--- golang/正则替换.go
+++ golang/正则替换.go
@@ -9,18 +9,17 @@ func main() {
 	const a = "abcdefg"
 	pat := "abc"
 
-	re,_ := regexp.Compile(pat)
+	re, _ := regexp.Compile(pat)
 	fmt.Println(re)
 
 	//将匹配到的部分进行替换
-	str := re.ReplaceAllString(a,"AAA")
+	str := re.ReplaceAllString(a, "AAA")
 	fmt.Println(str)
 
 	//参数为函数时
-	f := func (s string) string {
+	f := func(s string) string {
 		return "AAA"
 	}
-	str2 := re.ReplaceAllStringFunc(a,f)
+	str2 := re.ReplaceAllStringFunc(a, f)
 	fmt.Println(str2)
 }
-
