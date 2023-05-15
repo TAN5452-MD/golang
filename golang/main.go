d54174fc2e0a397850b2ffccf90775b1bce3e35c
TAN5452-MD <seesawgzz@gmail.com>
2023-05-15 17:46:21 +0800

    study

--- golang/main.go
+++ golang/main.go
@@ -1,27 +1,15 @@
 package main
 
-type Simpler interface {
-	Get() int
-	Set(int)
-}
-type Simple struct {
-	num int
-}
-
-func (s Simple) Get() int {
-	return s.num
-}
-
-// 如果不使用引用的话只是值传递并不能真正修改到值
-func (s *Simple) Set(num int) {
-	s.num = num
-}
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var a = new(Simple)
-	a.num = 1
-	var b Simpler
-	if _, ok := b.(a); ok {
-		return
-	}
+	var x = 1.23213
+	fmt.Println(reflect.TypeOf(x), reflect.ValueOf(x))
+	v := reflect.ValueOf(&x)
+	v = v.Elem()
+	v.SetFloat(2.3)
+	fmt.Println(v)
 }
--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-var plusOne = function(digits) {
-    let res = digits.join("")
-    ans = BigInt(res) + 1n
-    return String(ans).split("")
- 
- }
-console.log( plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]));
+let a = undefined
+let b = 0 ?? 2
+console.log(b);
